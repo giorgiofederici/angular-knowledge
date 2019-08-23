14dd85f9a347b1cdbd146db97e1a435934f365a1
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-23 15:37:22 +0200

    Added material component

--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '../material-module/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/topics/topics.module.ts
+++ src/app/topics/topics.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { TopicsComponent } from './containers/topics/topics.component';
 import { TopicComponent } from './components/topic/topic.component';
+import { MaterialModule } from '../material-module/material.module';
 
 const ROUTES: Routes = [
   {
@@ -12,7 +13,7 @@ const ROUTES: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  imports: [CommonModule, RouterModule.forChild(ROUTES), MaterialModule],
   declarations: [TopicsComponent, TopicComponent]
 })
 export class TopicsModule {}
